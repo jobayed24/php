c8e6ca40bbb2de85a8f8a51761393aa50cfe92ca
jobayed24 <jkhouse117@gmail.com>
2020-12-07 22:57:36 +0600

    save array anywhere and reuse it by php serialize funciotns and json encode

--- array.php
+++ array.php
@@ -1,2 +1,17 @@
 <?php
-multidimensional array i know how it's works and also if i want to make a string covert into an array
+$data=[
+    "fname"=>"jobayed",
+    "lname"=>"hossen",
+    "section"=>"B",
+    "roll"=>"22"
+];
+
+$serialize=serialize($data);   ///use serialze functions to save array data anywhere but it's useablitly isn't like json encode 
+
+$unserialize=unserialize($serialize);
+print_r($unserialize);
+
+$en=json_encode($data);         ///use json encode and decode for save data an array of php but need always json for it's two useabilit....i can use this valu in php also i can JS
+$de=json_decode($en,true);
+
+print_r($de);
