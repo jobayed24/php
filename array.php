f9be11e037d2d87d07a9a5109d0466057375c7cd
jobayed24 <jkhouse117@gmail.com>
2020-12-07 22:38:41 +0600

    mulidimentional array with explod functions

--- array.php
+++ array.php
@@ -1,15 +1,2 @@
 <?php
-$name="jobayed, tanvir, shakawat, nuravai";      ///convert string to array and array to string 
-$ex=explode(', ',$name);
-print_r($ex);
-
-$jo=join(", ",$ex);
-echo $jo;
-
-
-$vergin="vargin2, vargin3, vargin4, vargin5,vargin6,vargin7";  ///preg slit used for detect mutiple delimeter
-
-$pre=preg_split("/(, |,)/",$vergin);
-
-print_r($pre);
-echo count($pre);
+multidimensional array i know how it's works and also if i want to make a string covert into an array
