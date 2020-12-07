87ac90663182bc10acde43f540bde73815cd5481
jobayed24 <jkhouse117@gmail.com>
2020-12-07 08:10:36 +0600

    for adding and remove value in an array first and last also replace value

--- array.php
+++ array.php
@@ -6,12 +6,26 @@ $students=[
     "tanvir",
     "rasel",
 ];
+///add value in an array in the last 
+$students[]="rayhan";
 
-$n= count($students);
+//remove last value from the array...
+$po=array_pop($students);
+$po=array_pop($students);
+
+// remvoe first value from the array
+$si=array_shift($students);
+
+//adding value in first of array value
+$un=array_unshift($students,"23");
+
+//adding value in the last of array
+$pu=array_push($students,"nuru vai");
+
+//replace value in an array by indexing
+$in=$students[0]="khondakar";
+
+$n= count($students);           //for loop use for print all of value from array  remember that it's very important
 for($i=0;$i<$n;$i++){
-    echo $students[$i]."\n";
-}
-echo " "."\n";
-for($i=$n-1;$i>=0;$i--){
     echo $students[$i]."\n";
 }
