969712c56e24e45c5efe750eb8c58dc48efacdb6
jobayed24 <jkhouse117@gmail.com>
2020-12-08 10:54:53 +0600

    array slice use for extract some keys and values from an plain,associative array

--- array.php
+++ array.php
@@ -1,19 +1,17 @@
 <?php
-$person;
-if(isset($person)){
-    echo "name is set";
-}else{
-    echo "name is empty";
-}
-echo "\n";
-if(empty($person)){
-    echo "name is empty";
-}else{
-    echo "name is\'t not empty";
-}
-echo "\n";
-if(isset($person) && (is_numeric($person) || $person!='')){
-    echo "name is set and it's not an empty";
-}else{
-    echo "name is not set or empty";
-}
+
+//findout some value from an plain array by array slic
+
+$data=['tanvir','rasel','shakawat','nuru vai','khondakar'];
+
+$dataslic=array_slice($data,-3,-1,true);
+
+print_r($dataslic);
+
+//findout or extrat some value from an associative array by array slic
+
+$dataSlic=['a'=>23,'v'=>34,'g'=>55,'h'=>35,'t'=>45,23=>45];     ///when array keys have numeric key if want to carry the index of associative keys then must use true in an array slic
+
+$dataslic=array_slice($dataSlic,3,null,true);
+
+print_r($dataslic);
