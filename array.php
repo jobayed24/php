0e6e09a5060f69eeec42b7419c8301285cd0285c
jobayed24 <jkhouse117@gmail.com>
2020-12-08 10:17:45 +0600

    unset functions use for remove data from associative array

--- array.php
+++ array.php
@@ -1,32 +1,11 @@
 <?php
 $person=['fname'=>'jobayed','lanme'=>'khondakar'];
 
-//dep copy ,copy by value
+//unset function use for remove data  form an associative array
 
-$newperson=$person;
-
-$newperson['fname']='rasel';
-
-print_r($newperson);
 
 print_r($person);
 
-//copy by reference or shallow copy
-
-$person2=&$person;
-
-$person2['fname']='nuru vai';
-
-print_r($person2);
-
-print_r($person);
-
-
-function newPerson($person){   ///shawllow copy deep copy by a function
-    $person['fname']='ruddra';
-    print_r($person);
-} 
-
-newPerson($person);
+unset($person['fname']);
 
 print_r($person);
