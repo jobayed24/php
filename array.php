fe39c3a09c9e11278cfa6d957a490f8f8584bda1
jobayed24 <jkhouse117@gmail.com>
2020-12-09 23:03:58 +0600

    search key or value in an array is it exit by is_array array_search ,key_exit

--- array.php
+++ array.php
@@ -1,26 +1,22 @@
 <?php
+///seach key or value in array by using array_search,,,in_array,,,array_exit
+
 $fruits=['p'=>'pineaple',"j"=>'jeckfruit','t'=>'tomato','l'=>'letu32s','a'=>'apple'];
 $numbers=[2,74,33,32,24,67,22,55,43,66];
 $alphabaets=['apple','Apple','cucumber','Cucumber','beat','Beat'];
 
-sort($alphabaets,SORT_STRING | SORT_FLAG_CASE);
-
-print_r($alphabaets);
-
-krsort($fruits);      ///sorting by sort /ksort/foreah for orginnial key reminder...
-
-print_r($fruits);
-
-$array_values=array_values($fruits);
-
-for($i=0; $i<count($fruits); $i++){
-    echo $fruits[$i]."\n";
+//// in index array if value is twice or thrice or more it print first value index .....
+if(in_array(2,$numbers)){
+    echo "this number is exit" ;
+}else{
+    echo "this number is not exit";
 }
-for($i=0; $i<count($array_values); $i++){
-    echo $array_values[$i]."\n";
-}
+echo "\n";
+$exit=array_search(33,$numbers);
+echo $exit;
 
 echo "\n";
-foreach($fruits as $key=>$value){
-    echo $key.'='.$value."\n";
+
+if(key_exists("p",$fruits)){
+    echo "key is exit";
 }
