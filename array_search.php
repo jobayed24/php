c6bea1d67f88eabaf5b43405cab92a652e931c18
jobayed24 <jkhouse117@gmail.com>
2021-08-10 22:43:59 +0600

    use for values in array and it's return with index number

---
+++ array_search.php
@@ -0,0 +1,7 @@
+<?php
+$array_search=array("tanvir","jackfruits","mango","pineapple");
+$array_search_multi=array(1=>"tanvir",2=>"shakawat",3=>"taiub");
+$array_search=array_search("shakawat",$array_search_multi);
+
+
+print_r($array_search);
