2dbe2591b985af304246c0f0c40cf499a8417537
jobayed24 <jkhouse117@gmail.com>
2021-08-11 22:16:40 +0600

    current and next and prev and end use for detect arrays values

---
+++ current.php
@@ -0,0 +1,12 @@
+<?php
+
+$fruits=array("pineapple","jackfruits","mango","orange");
+
+print_r(current($fruits)."\n");
+print_r(next($fruits)."\n");
+print_r(current($fruits)."\n");
+print_r(end($fruits)."\n");
+
+print_r(prev($fruits));
+
+
